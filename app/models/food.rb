b1198d19f52e9591cfffb8ac5043aa4279764963
Hombre2014 <chamkoriyski@gmail.com>
2022-04-20 15:34:45 +0200

    Change the Food model associations

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
-  belongs_to :user, class_name: 'User'
-  has_many :recipe_foods
+  belongs_to :user
+  has_many : recipes, through: :recipe_foods
 
   validates :name, length: { in: 1..250 }, presence: true, allow_blank: false
   validates :measurementUnit, length: { in: 1..250 }, presence: true, allow_blank: false
