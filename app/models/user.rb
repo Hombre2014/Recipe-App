43b08961978c555074789b52735a12949d6cd460
Hombre2014 <chamkoriyski@gmail.com>
2022-04-22 14:42:08 +0200

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
 
